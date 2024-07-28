dec1fa445c79eb49b5436d32885875196c8c152a
KRitchie823 <158782034+KRitchie823@users.noreply.github.com>
2024-07-28 16:02:34 -0400

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,6 @@ export default function Weather() {
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-      icon: "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png",
     });
   }
   function handleSubmit(event) {
